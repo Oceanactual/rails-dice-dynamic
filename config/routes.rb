2dcbdda23c49a8f436189c075c0c7a303098128c
Palmer Lynch <135549235+Oceanactual@users.noreply.github.com>
2023-07-21 18:30:35 +0000

    App loaded in fuly w/ errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-
+  get("/", {:controller => "roll", :action => "home"})
+  get("/dice/2/6", {:controller => "roll", :action => "2d6"})
+  get("/dice/2/10", {:controller => "roll", :action => "2d10"})
+  get("/dice/1/20", {:controller => "roll", :action => "1d20"})
+  get("/dice/5/4", {:controller => "roll", :action => "5d4"})
 end
